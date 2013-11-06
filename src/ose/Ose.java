f707490d9ea76a0712cc199ccf5eb9e67d9ca90b
hamihad <hadjara.hamidou.sio@gmail.com>
2013-11-06 15:03:48 +0100

    version0.2composition

---
+++ src/ose/Ose.java
@@ -0,0 +1,21 @@
+package ose;
+
+import fr.mertzel.ose.vue.FenetrePrincipale;
+import fr.mertzel.ose.vue.PlanSalle;
+import fr.metzel.ose.controleur.Controleur;
+
+/** Application OSE
+ * @since Janvier 2013
+ * @author xilim
+ * @version 0.2
+ */
+ public class Ose {
+	 
+	/** Point d'entrée dans le programme
+	 */
+	public static void main(String [] args){
+		PlanSalle modele = new PlanSalle("Sans Nom") ;
+		FenetrePrincipale vue = new FenetrePrincipale(modele) ;
+		new Controleur(vue,modele) ;
+	}
+}
